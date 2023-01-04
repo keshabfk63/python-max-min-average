7902f782474d97f12ed907c842a2b8db1928833e
Keshab Karmacharya <117242793+keshabfk63@users.noreply.github.com>
2023-01-04 17:19:17 +0545

    Add files via upload

---
+++ max and min.py
@@ -0,0 +1,10 @@
+li=list(map(int,input("enter data for list:").split(",")))
+print(li)
+max1=max(li)
+min1=min(li)
+average=sum(li)/len(li)
+print("the maximum value is:",max1)
+print("the minimum value is:",min1)
+print("the average is :",average)
+
+
